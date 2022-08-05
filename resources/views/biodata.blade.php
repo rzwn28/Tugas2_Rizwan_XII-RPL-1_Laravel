f5094dd3b0c46b900ff23ead397f2c509b8fa59b
rzwan28 <rizwanfebriana2825@gmail.com>
2022-08-05 18:48:00 +0700

    first commit

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Coba | Biodata </title>
+</head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"href="css/stylee.css">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Rizwan Blog</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="biodata">Biodata</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="halaman 3">Halaman 3</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="halaman 4">Halaman 4</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="row justify-content-center align-items-center text-center">
+  <div class="col">
+  <h1>Biodata</h1>
+
+<h3>Nama : Rizwan Pebriana </h3>
+<h4>Kelas : XII RPL 1</h4>
+<h5>Alamat : Blok Sukawening</h5>
+<img src="img/rizwan2.jpg" alt="Rizwan" width="200" class="rounded-circle img-thumbnaill"/>
+
+  </div>
+</div>
+</body>
+</html>
---
+++ resources/views/halaman 3.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style3.css">
+    <title>Halaman Blog</title>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"href="css/stylee.css">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+</head>
+<nav class="navbar navbar-expand-lg bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Rizwan Blog</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="biodata">Biodata</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="halaman 3">Halaman 3</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="halaman 4">Halaman 4</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<body>
+
+</head>
+<body>
+    <h1>Halaman 3</h1>
+</body>
+</html>
